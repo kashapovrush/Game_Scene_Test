6cae85a154e042410af9b9383af87bed77cafcce
Rushan Kashapov <kashapovrush@mail.ru>
2023-12-18 19:07:24 +0300

    implementation start game screen finished (18.12.23)

--- app/src/main/java/com/kashapovrush/gamescenetest/presentation/GameOverActivity.kt
+++ app/src/main/java/com/kashapovrush/gamescenetest/presentation/GameOverActivity.kt
@@ -5,11 +5,24 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.kashapovrush.gamescenetest.R
+import com.kashapovrush.gamescenetest.databinding.ActivityGameOverBinding
 
 class GameOverActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityGameOverBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_game_over)
+        binding = ActivityGameOverBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonDoubleReward.setOnClickListener {
+            startActivity(GameActivity.newIntent(this))
+        }
+
+        binding.buttonToHome.setOnClickListener {
+            startActivity(StartActivity.newIntent(this))
+        }
     }
 
     companion object {
