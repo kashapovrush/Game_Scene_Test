0d8eae46cd106562b7f2acb4cca3e458e5f8151e
Rushan Kashapov <kashapovrush@mail.ru>
2023-12-18 17:05:18 +0300

    create start screen finished(18.12.23)

--- app/src/main/java/com/kashapovrush/gamescenetest/presentation/StartActivity.kt
+++ app/src/main/java/com/kashapovrush/gamescenetest/presentation/StartActivity.kt
@@ -1,9 +1,10 @@
-package com.kashapovrush.gamescenetest
+package com.kashapovrush.gamescenetest.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kashapovrush.gamescenetest.R
 
-class MainActivity : AppCompatActivity() {
+class StartActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
