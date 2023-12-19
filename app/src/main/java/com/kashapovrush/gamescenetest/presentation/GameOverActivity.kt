7498867f1d1617993be9855bd5e07e3fff30ad67
Rushan Kashapov <kashapovrush@mail.ru>
2023-12-19 16:57:34 +0300

    implementation game over activity finished (19.12.23)

--- app/src/main/java/com/kashapovrush/gamescenetest/presentation/GameOverActivity.kt
+++ app/src/main/java/com/kashapovrush/gamescenetest/presentation/GameOverActivity.kt
@@ -5,6 +5,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.util.Log
+import android.view.View
 import com.kashapovrush.gamescenetest.R
 import com.kashapovrush.gamescenetest.databinding.ActivityGameOverBinding
 
@@ -16,6 +18,21 @@ class GameOverActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityGameOverBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val score = requireNotNull(intent.getIntExtra("KEY", 0))
+        binding.score.text = score.toString()
+        if (score == 100) {
+            binding.textWon.text = "[GREAT! YOU WON!]"
+            binding.finishedText.text = "[CONGRATULATION!]"
+        } else if (score in 50..99) {
+            binding.cup.visibility = View.GONE
+            binding.textWon.text = "[GOOD RESULT]"
+            binding.finishedText.text = "[VERY GOOD]"
+        } else {
+            binding.cup.visibility = View.GONE
+            binding.finishedText.text = "[FINISHED]"
+            binding.textWon.text = "[BAD RESULT]"
+        }
 
         binding.buttonDoubleReward.setOnClickListener {
             startActivity(GameActivity.newIntent(this))
